cf121e6c7fa92308907dc5ae5e59205cc9654fc9
Emma Seymour <emma_seymour@icloud.com>
2024-07-22 18:52:29 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Sec.Hand</h1>
+      <img src="/src/assets/images/midlayer.jpg" />
+    </div>
+  )
+}
+
+export default App
